f3375db65c3e7681631d3027bb13373a9dfc20e5
PapaKramb <guccigang5554@gmail.com>
2020-08-28 21:55:24 +0900

    Update posts controller, add new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,23 +11,41 @@ class PostsController < ApplicationController
   end
 
   def new
-
+  
   end
 
   def create
-
+    @posts = current_user.posts.new(posts_params)
+    if @posts.save
+      redirect_to @posts
+    else
+      render action: 'new'
+    end
   end
 
   def edit
-
+    @posts = Post.find(params[:id])
   end
 
   def update
-
+    @posts = Post.find(params[:id])
+    if @posts.update
+      redirect_to @posts
+    else
+      render action 'edit'
+    end
   end
 
   def destroy
+    @posts = Post.find(params[:id])
+    @posts.destroy
+    redirect_to post_path
+  end
 
+  private
+
+  def posts_params
+    params.require(:posts).permit(:title, :body)
   end
   
 end
