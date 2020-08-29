cf0a82d117fa8e17a8887821cadb97a4b73467af
PapaKramb <guccigang5554@gmail.com>
2020-08-29 09:59:33 +0900

    Add form for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,17 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, :only => [:create]
 
   def create
-    @posts = Post.find(params[:id])
+    @posts = Post.find(params[:post_id])
     @c = @posts.comments.new(comments_params)
-    @c.author = current_username.current_username
+    @c.author = current_user.username
     @c.save
+
+    redirect_to post_path(@posts)
   end
 
   private
 
   def comments_params
-    params.require(:comments).permit(:body)
+    params.require(:comment).permit(:author, :body)
   end
 end
